cf87f49b43338efd2b9a4e2a737726aeef2baa59
Ali Zulqarnain <warraichleopard@gmail.com>
2024-05-04 23:59:55 +0500

    Setup prisma

---
+++ prisma/migrations/20240504185909_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
