88840dba2a5bc1306f122539ef5e754c92cf1630
Ali Zulqarnain <warraichleopard@gmail.com>
2024-09-21 20:04:47 +0500

    feat: written auth server with authenticator and password hashing

--- app/server/auth.server.ts
+++ app/server/auth.server.ts
@@ -1,6 +1,8 @@
 import { Authenticator } from "remix-auth";
 import { sessionStorage } from "./sessions.server";
 import { FormStrategy } from "remix-auth-form";
+import * as bcrypt from "bcrypt";
+import { prisma } from "./db.server";
 import { User } from "@prisma/client";
 
 // Create an instance of the authenticator, pass a generic with what
@@ -10,9 +12,36 @@ export const authenticator = new Authenticator<User>(sessionStorage);
 // Tell the Authenticator to use the form strategy
 authenticator.use(
     new FormStrategy(async ({ form }) => {
-        const email = form.get("email");
-        const password = form.get("password");
-        const user = await login(email, password);
+        const name = form.get("name")?.toString() || "";
+        const email = form.get("email")?.toString() || "";
+        const password = form.get("password")?.toString() || "";
+
+        // You can validate the inputs however you want
+        if (!email || !password) {
+            throw new Error("Email and password are required");
+        }
+
+        // And if you have a password you should hash it
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // And finally, you can find, or create, the user
+        let user = await prisma.user.findFirst({
+            where: {
+                email,
+            },
+        });
+
+        if (!user) {
+            user = await prisma.user.create({
+                data: {
+                    email,
+                    password: hashedPassword,
+                    name,
+                },
+            });
+        }
+
+        // And return the user as the Authenticator expects it
         // the type of this user must match the type you pass to the Authenticator
         // the strategy will automatically inherit the type if you instantiate
         // directly inside the `use` method
